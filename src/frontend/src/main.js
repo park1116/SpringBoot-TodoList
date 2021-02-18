9eb649c66dc3acd7b9a0d5b25b11c9a80fb5adfa
park1116 <park_1116@naver.com>
2021-02-19 01:00:33 +0900

    Vue를 활용한 화면 출력

---
+++ src/frontend/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+//import App from './App.vue'
+//import router from './router'
+
+Vue.config.productionTip = false
+
+//new Vue({
+//	el:'#app',
+//	data: {
+//		message: '안녕하세요! Vue.js!'
+//	},
+//	router,
+//	components:{App},
+//	template:'<App/>'
+//})
