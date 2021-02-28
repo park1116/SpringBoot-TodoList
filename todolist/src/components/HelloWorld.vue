a8a617dddea3abde0796dc9bfc6ac6ecea3bed29
park1116 <park_1116@naver.com>
2021-02-28 17:28:28 +0900

    Example_Vue Router

---
+++ todolist/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <p>Hello World~!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
